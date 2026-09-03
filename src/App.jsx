a9f8da22c5648ce9ab35e50b4a194fbb3123e581
mohamed elkheateb <156317154+mohamed10987@users.noreply.github.com>
2026-09-03 16:05:46 +0200

    feat: update facebook link

--- src/App.jsx
+++ src/App.jsx
@@ -1113,7 +1113,7 @@ export default function App() {
                 الشركة الرائدة في جمهورية مصر العربية والشرق الأوسط لتصنيع ماكينات نشر وقص الرخام والجرانيت، جلايات الشرائح، والأوناش العلوية والساحات حتى 100 طن بأعلى معايير الجودة والصلابة.
               </p>
               <div className="footer-social-links">
-                <a href="https://www.facebook.com/1ahmedfekry/" target="_blank" rel="noopener noreferrer" className="social-btn" aria-label="فيسبوك">
+                <a href="https://www.facebook.com/ic.marble.machinery" target="_blank" rel="noopener noreferrer" className="social-btn" aria-label="فيسبوك">
                   <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
                 </a>
                 <a href="https://www.instagram.com/p/DLNjbixITRX/" target="_blank" rel="noopener noreferrer" className="social-btn" aria-label="انستغرام">
